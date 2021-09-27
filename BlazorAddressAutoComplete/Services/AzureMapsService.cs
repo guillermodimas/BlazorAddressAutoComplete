bc38e0525de91763e800a9a06bb3a1a22a056244
Guillermo Dimas <dimasg@mdpls.org>
2021-09-27 15:22:11 -0400

    Add project files.

---
+++ BlazorAddressAutoComplete/Services/AzureMapsService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using BlazorAddressAutoComplete.Models;
+
+namespace BlazorAddressAutoComplete.Services
+{
+    public class AzureMapsService : IAzureMapsService
+    {
+        private HttpClient _apiClient;
+        private readonly IConfiguration _config;
+        private readonly ILogger<AzureMapsService> _logger;
+
+        public AzureMapsService(IConfiguration config, ILogger<AzureMapsService> logger) //constructor
+        {
+            _config = config;
+            _logger = logger;
+            InitializeClient();
+        }
+        private void InitializeClient()
+        {
+            string api = _config["AzureMaps:Domain"]; //ConfigurationManager.AppSettings["api"]; //api URL from app.config
+            _apiClient = new HttpClient();
+            _apiClient.BaseAddress = new Uri(api);
+            _apiClient.DefaultRequestHeaders.Accept.Clear();
+            _apiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        
+        public async Task<ResultsAutoComplete> AutocompleteAddress(string Address)
+        {
+
+            using (HttpResponseMessage response = await _apiClient.GetAsync($"/search/address/json?typeahead=true&api-version=1&query={Address}&language=en-US&lon=0&lat=0&countrySet=US&view=Auto&subscription-key={_config["AzureMaps:ApiKey"]}"))
+            {
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadFromJsonAsync<ResultsAutoComplete>();
+                }
+                else
+                {
+                    _logger.LogError(response.ReasonPhrase);
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+
+}
---
+++ BlazorAddressAutoComplete/Services/IAzureMapsService.cs
@@ -0,0 +1,11 @@
+﻿using BlazorAddressAutoComplete.Models;
+using System.Threading.Tasks;
+
+namespace BlazorAddressAutoComplete.Services
+{
+    public interface IAzureMapsService
+    {
+        Task<ResultsAutoComplete> AutocompleteAddress(string Address);
+     
+    }
+}
